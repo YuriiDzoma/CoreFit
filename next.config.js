6973ef57251651e092d8d1c0ed0b09023210683b
YuriiDzoma <hp_Yr8xoCiyd9HTYeklWGhXjSpbSU60xh4BiJoc>
2025-04-10 10:45:05 +0300

    upd 1.1

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+};
+
+module.exports = withPWA(nextConfig);
