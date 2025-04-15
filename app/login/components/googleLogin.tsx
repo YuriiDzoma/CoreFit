a8d772d59a985a0b73d27c8de0c15566598fc586
YuriiDzoma <hp_Yr8xoCiyd9HTYeklWGhXjSpbSU60xh4BiJoc>
2025-04-15 23:56:21 +0300

    upd 1.81

--- app/login/components/googleLogin.tsx
+++ app/login/components/googleLogin.tsx
@@ -4,11 +4,16 @@ import React from 'react';
 
 export default function GoogleLogin() {
     const handleLogin = async () => {
+        const redirectTo =
+            process.env.NODE_ENV === 'development'
+                ? 'http://localhost:3000'
+                : 'https://core-fit-ua.vercel.app';
+
         const supabase = createClient();
         await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
-                redirectTo: 'https://core-fit-ua.vercel.app', // жорстко, без `${window.location.origin}`
+                redirectTo: redirectTo
             },
         });
     };
