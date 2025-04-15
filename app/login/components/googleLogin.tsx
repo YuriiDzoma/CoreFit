7dcdaac2f6c849a8fb6eb4eca24f123b4dbde27c
YuriiDzoma <hp_Yr8xoCiyd9HTYeklWGhXjSpbSU60xh4BiJoc>
2025-04-15 19:02:23 +0300

    upd 1.66

--- app/login/components/googleLogin.tsx
+++ app/login/components/googleLogin.tsx
@@ -5,17 +5,20 @@ import React from "react";
 export default function GoogleLogin() {
     const handleLogin = async () => {
         const redirectTo =
-            typeof window !== 'undefined' && window.location.origin
-                ? `${window.location.origin}`
-                : 'https://core-fit-ua.vercel.app';
+            process.env.NODE_ENV === 'production'
+                ? 'https://core-fit-ua.vercel.app/auth/callback'
+                : 'http://localhost:3000/auth/callback';
+
         const supabase = createClient();
         await supabase.auth.signInWithOAuth({
             provider: 'google',
-            options: {
-                redirectTo,
-            },
+            options: { redirectTo },
         });
     };
 
-    return <button type={'button'} onClick={handleLogin}>Увійти через Google ТЕСТ</button>;
+    return (
+        <button type="button" onClick={handleLogin}>
+            Увійти через Google
+        </button>
+    );
 }
