d908e4ef0ec49990057c5255999182eeea8e879a
YuriiDzoma <hp_Yr8xoCiyd9HTYeklWGhXjSpbSU60xh4BiJoc>
2025-09-08 15:59:45 +0300

    upd 0.4.92

--- app/components/inputBox/inputBox.tsx
+++ app/components/inputBox/inputBox.tsx
@@ -1,6 +1,9 @@
+'use client';
 import styles from './inputBox.module.scss';
 import React from "react";
 import { FieldErrors, FieldError, UseFormRegisterReturn } from "react-hook-form";
+import {useAppSelector} from "../../hooks/redux";
+import {getIsDarkTheme} from "../../../store/selectors";
 
 type InputBoxProps = {
     errors: FieldErrors;
@@ -12,7 +15,6 @@ type InputBoxProps = {
     defaultValue?: string;
 };
 
-
 const InputBox: React.FC<InputBoxProps> = ({
                                                errors,
                                                name,
@@ -22,11 +24,21 @@ const InputBox: React.FC<InputBoxProps> = ({
                                                options,
                                                defaultValue = '',
                                            }) => {
+    const isDark = useAppSelector(getIsDarkTheme);
+
     return (
         <div className={styles.inputBox}>
             <label className={styles.inputBox__label} htmlFor={name}>{label}</label>
-            <input className={`${styles.inputBox__input} ${(errors[name] || errors['All']) ? styles.inputBox__error : ''}`} type={type}
-                   placeholder={placeholder} id={name} name={name} {...options} defaultValue={defaultValue}/>
+            <input
+                className={`${styles.inputBox__input} ${(errors[name] || errors['All']) ? styles.inputBox__error : ''}`}
+                type={type}
+                placeholder={placeholder}
+                id={name}
+                name={name}
+                {...options}
+                defaultValue={defaultValue}
+                style={{ color: isDark ? '#fff' : '#19355A' }}
+            />
             {errors && typeof errors[name] === 'object' && 'message' in errors[name]! && (
                 <p className="errorMsg">{(errors[name] as FieldError).message}</p>
             )}
